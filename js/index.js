eacf6fa159d05ab9520d58afaced641a3d7e53e8
Terrence <thenriquez25@outlook.com>
2016-06-17 02:56:54 -0400

    added changes for the annonmyous funcition

--- js/index.js
+++ js/index.js
@@ -21,10 +21,41 @@ $scope.categories = [
   {name: 'Family'}
 ];
 $scope.newTaskCategory = $scope.categories;
-$scope.addNew = function () {
-    if ($scope.newTaskDate == null ||
-      $scope.newTaskDate == '') {
+    $scope.addNew = function () {
+        if ($scope.newTaskDate == null || $scope.newTaskDate == '') {
+            $scope.taskItem.push({
+                description: $scope.newTask,
+                date: "No deadline",
+                complete: false,
+                category: $scope.newTaskCategory.name
+            })
+        } else {
+            $scope.taskItem.push({
+                description: $scope.newTask,
+                date: $scope.newTaskDate,
+                complete: false,
+                category: $scope.newTaskCategory.name
+            })
+        };
 
-      }
-}
-}
+        $scope.newTask = '';
+        $scope.newTaskDate = '';
+        $scope.newTaskCategory = $scope.categories;
+        localStorage.setItem('taskitems',
+        JSON.stringify($scope.taskItem))
+      };
+      $scope.deleteTask = function () {
+        var completedTask = $scope.taskItem;
+        $scope.taskItem = [];
+        angular.forEach(completedTask, function (taskItem) {
+            if (!taskItem.complete) {
+                $scope.taskItem.push(taskItem);
+            }
+        });
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    };
+
+    $scope.save = function () {
+        localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+    }
+});
